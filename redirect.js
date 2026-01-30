618c570271c6b286e9adf1ad136a857b1f2a4c86
jlmf12 <jlmf12@gmail.com>
2026-01-30 19:38:03 +0100

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("registroForm");
+  if (!form) return;
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const destino = localStorage.getItem("destino_final");
+    const origen = localStorage.getItem("origen_final") || "desconocido";
+    const nombre = form.nombre.value;
+    const email = form.email.value || "";
+    const fecha = new Date().toISOString();
+
+    await fetch("https://api.github.com/repos/jlmf12/redirect-proxy/dispatches", {
+      method: "POST",
+      headers: {
+        "Accept": "application/vnd.github+json"
+      },
+      body: JSON.stringify({
+        event_type: "registro_click",
+        client_payload: {
+          nombre,
+          email,
+          origen,
+          fecha,
+          destino
+        }
+      })
+    });
+
+    window.location.href = destino;
+  });
+});
